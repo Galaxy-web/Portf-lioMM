53094a8fb5b0ec0c72c3914488ab0a9c8144bb40
Desenvolvedor-Webs <146100220+Galaxy-web@users.noreply.github.com>
2023-10-24 10:42:32 -0300

    Upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("open-button").addEventListener("click", function() {
+    document.getElementById("sidebar").style.left = "0";
+});
+
+document.getElementById("close-button").addEventListener("click", function() {
+    document.getElementById("sidebar").style.left = "-250px";
+});
+
